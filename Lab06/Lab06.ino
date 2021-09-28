d6752379dc675fe475aca1bb3c766c1bec7426bf
Juan Mora <juan@nimble.gt>
2021-09-27 23:30:56 -0600

    Initial Commit (Lab 7)

---
+++ Lab06/Lab06.ino
@@ -0,0 +1,259 @@
+
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_memmap.h" // https://github.com/yuvadm/tiva-c/blob/master/inc/hw_memmap.h
+
+#define p1 PUSH1
+#define p2 PUSH2
+
+// j1
+#define j11 PB_5
+#define j12 PB_0
+#define j13 PB_1
+#define j14 PE_4
+#define j15 PE_5
+#define j16 PB_4
+#define j17 PA_5
+#define j18 PA_6
+#define j19 PA_7
+
+// j2
+
+#define j21 PB_2
+#define j22 PE_0
+#define j23 PF_0
+//#define j24 PE_4
+#define j25 PB_7
+#define j26 PB_6
+#define j27 PA_4
+#define j28 PA_3
+#define j29 PA_2
+#define j49 PD_7
+
+//bool playing = false;
+int player1 = 0;
+int player2 = 0;
+
+int p1state = 0;
+int p2state = 0;
+
+int p1old = 0;
+int p2old = 0;
+
+bool ganador = false;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  pinMode(p1, INPUT_PULLUP);
+  pinMode(p2, INPUT_PULLUP);
+
+  // j1
+  pinMode(j11, INPUT);
+  pinMode(j12, INPUT);
+  pinMode(j13, INPUT);
+  pinMode(j14, INPUT);
+  pinMode(j15, INPUT);
+  pinMode(j16, INPUT);
+  pinMode(j17, INPUT);
+  pinMode(j18, INPUT);
+  pinMode(j19, INPUT);
+
+  // j2
+
+  pinMode(j21, INPUT);
+  pinMode(j22, INPUT);
+  pinMode(j23, INPUT);
+  pinMode(j25, INPUT);
+  pinMode(j26, INPUT);
+  pinMode(j27, INPUT);
+  pinMode(j28, INPUT);
+  pinMode(j29, INPUT);
+  pinMode(j49, INPUT);
+
+  // Failed attempt
+  //  GPIOPadConfigSet(GPIO_PORTA_BASE,
+  //  GPIODirModeSet(GPIO_PORTA_BASE, 0x0 ,GPIO_DIR_MODE_IN);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  // put your main code here, to run repeatedly:
+  p1state = digitalRead(p1);
+
+  if (p1state != p1old)
+  {
+    if (p1state == LOW)
+    {
+      digitalWrite(RED_LED, HIGH);
+      player1++;
+
+      Serial.print("Player 1:  ");
+      Serial.println(player1);
+    }
+    else
+    {
+      digitalWrite(RED_LED, LOW);
+    }
+    p1old = p1state;
+  }
+
+  p2state = digitalRead(p2);
+
+  if (p2state != p2old)
+  {
+    if (p2state == LOW)
+    {
+      digitalWrite(GREEN_LED, HIGH);
+      player2++;
+
+      Serial.print("Player 2:  ");
+      Serial.println(player2);
+    }
+    else
+    {
+      digitalWrite(GREEN_LED, LOW);
+    }
+    p2old = p2state;
+  }
+
+  
+  if (!ganador)
+  {
+
+    switch (player1)
+    {
+    case 0:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 1:
+      digitalWrite(j11, HIGH);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 2:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, HIGH);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 3:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, HIGH);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 4:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, HIGH);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 5:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, HIGH);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 6:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, HIGH);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 7:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, HIGH);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, LOW);
+      break;
+    case 8:
+      digitalWrite(j11, LOW);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, HIGH);
+      digitalWrite(j19, LOW);
+      break;
+    case 9:
+      digitalWrite(j11, HIGH);
+      digitalWrite(j12, LOW);
+      digitalWrite(j13, LOW);
+      digitalWrite(j14, LOW);
+      digitalWrite(j15, LOW);
+      digitalWrite(j16, LOW);
+      digitalWrite(j17, LOW);
+      digitalWrite(j18, LOW);
+      digitalWrite(j19, HIGH);
+      break;
+    case 10:
+      digitalWrite(j11, HIGH);
+      digitalWrite(j12, HIGH);
+      digitalWrite(j13, HIGH);
+      digitalWrite(j14, HIGH);
+      digitalWrite(j15, HIGH);
+      digitalWrite(j16, HIGH);
+      digitalWrite(j17, HIGH);
+      digitalWrite(j18, HIGH);
+      digitalWrite(j19, HIGH);
+      Serial.println("Jugador 1 ha ganado");
+      ganador = true;
+      break;
+    }
+  }
+
+}
---
+++ Lab7/Lab7.ino
@@ -0,0 +1,150 @@
+/*
+  SD card test
+
+ This example shows how use the utility libraries on which the'
+ SD library is based in order to get info about your SD card.
+ Very useful for testing a card when you're not sure whether its working or not.
+
+ The circuit:
+  * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
+ ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
+ ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
+ ** CS - depends on your SD card shield or module.
+     Pin 4 used here for consistency with other Arduino examples
+
+
+ created  28 Mar 2011
+ by Limor Fried
+ modified 9 Apr 2012
+ by Tom Igoe
+ */
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
+#define file1 "A.txt"
+#define file2 "B.txt"
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+const int chipSelect = PA_3; //cs PIN
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+ 
+  SPI.setModule(0);
+  if (!SD.begin(PA_3)){
+    Serial.println("Error reading SD");
+  }
+
+  Serial.print("\nInitializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
+  // or the SD library functions will not work.
+  pinMode(PA_3, OUTPUT);     // change this to 53 on a mega
+
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card is inserted?");
+    Serial.println("* Is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+
+
+  Serial.println("\nFiles found on the card: ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  // root.ls(LS_R | LS_DATE | LS_SIZE);
+  root.ls(LS_R);
+  
+  Serial.println("Escriba el nombre del archivo que desea leer: ");
+}
+
+int opt = 0;
+
+File dataFile;
+
+void loop(void) {
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    opt = Serial.read();
+    Serial.print("Opening file ");
+    Serial.print(opt);
+
+    if(opt == 48) {
+          dataFile= SD.open(file1);
+    } else if (opt == 49) {
+          dataFile= SD.open(file2);
+    } else {
+      Serial.println("Opcion no existe");
+    }
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    while (dataFile.available()) {
+      Serial.write(dataFile.read());
+    }
+    dataFile.close();
+    Serial.println("\nEscriba el nombre del archivo que desea leer: ");
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening file");
+    Serial.println("Escriba el nombre del archivo que desea leer: ");
+  }
+  }
+} 
